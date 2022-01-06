162bdaddd7143e2e700956180c35052bfbf789cd
Amy2022b <97150918+Amy2022b@users.noreply.github.com>
2022-01-06 12:40:20 +0530

    Update TestClass.java

--- TestRepoProject/src/mytest/com/TestClass.java
+++ TestRepoProject/src/mytest/com/TestClass.java
@@ -12,6 +12,7 @@ public class TestClass {
 		// Merging the code of the branch Defect2 to the Master branch by the team lead
 		// Merging the code of the branch Defect1 to the Master branch by the team lead
        System.out.println("this is my second test of merging the code of branch Defect1");
+	System.out.println("this is my third test of merging the code of branch Defect1");
 	}
 
 }
