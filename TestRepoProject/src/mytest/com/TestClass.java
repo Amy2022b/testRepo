4b913aeb9f60c2a2463e9690d4d732f23ae88d97
Amy2022b <97150918+Amy2022b@users.noreply.github.com>
2022-01-06 12:34:06 +0530

    Update TestClass.java

--- TestRepoProject/src/mytest/com/TestClass.java
+++ TestRepoProject/src/mytest/com/TestClass.java
@@ -1,17 +1,3 @@
-
-package mytest.com;
-
-public class TestClass {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      System.out.println("this is my test2");
-      System.out.println("this is my test4");
-      // Merging the code of the branch Defect1
-	}
-
-}
-
 package mytest.com;
 
 public class TestClass {
@@ -22,9 +8,9 @@ public class TestClass {
       System.out.println("this is my test4");
       System.out.println("this is my test5");
 		// Merging the code of the branch Defect2
+		// Merging the code of the branch Defect1
 		// Merging the code of the branch Defect2 to the Master branch by the team lead
 		// Merging the code of the branch Defect1 to the Master branch by the team lead
 	}
 
 }
-
